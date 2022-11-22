1311318b6bed0b2ebd0e42936d1fca7dfab6bb63
Constantino Abba <cons_benjamin9@outlook.com>
2022-11-22 12:36:41 -0300

    falta crud y testing

--- client/src/components/Paginado/Paginado.jsx
+++ client/src/components/Paginado/Paginado.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import './Paginado.css';
 
-export default function Paginado ({dogsPerPage, allDogs, paginado}) {
-    const pageNumbers = []
+export default function Paginado ({dogsPerPage, allDogs, paginado, currentPage}) {
+    const pageNumbers = []; // array usado para guardar la cantidad de páginas necesarias
 
     for (let i=1; i<=Math.ceil(allDogs/dogsPerPage); i++){
         pageNumbers.push(i)
-    }
+    };
+
+    
 
     return(
         <nav>
             <ul className='pagination'>
-                {pageNumbers  && pageNumbers.map(number =>(
-                    <li>
-                    <a onClick={() => paginado(number)}>{number}</a>
-                    </li>
-                ))}
+                <div>
+                    <button className='backnext1' onClick={currentPage> 1 ? () => paginado(currentPage-1): null}>❮❮</button>
+                    {pageNumbers  && pageNumbers.map(number =>(
+                        <li>
+                        <a onClick={() => paginado(number)}>{number}</a>
+                        </li>
+                    ))}
+                    <button className='backnext2' onClick={currentPage !== pageNumbers.length ?() => paginado(currentPage+1): null}>❯❯</button>
+                </div>
             </ul>
         </nav>
     )
