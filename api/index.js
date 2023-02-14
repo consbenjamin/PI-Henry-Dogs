ce99c9a75ceff1a4e71b54d4e95468b666afe9e3
Constantino Abba <cons_benjamin9@outlook.com>
2023-02-14 17:27:45 -0300

    restructuring deploy

--- api/index.js
+++ api/index.js
@@ -20,10 +20,11 @@
 
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-
+require('dotenv').config();
+const { PORT } = process.env;
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('server is listening on port 3001'); // eslint-disable-line no-console
+conn.sync({ force: true }).then(() => {
+  server.listen(PORT, () => {
+    console.log('server is listening at',process.env.PORT); // eslint-disable-line no-console
   });
 });
